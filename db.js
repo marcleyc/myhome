44505796e3fd36d2604d6edaea86f12921b4e531
marcleyc <marcleycc@gmail.com>
2020-09-28 17:36:42 +0100

    Add files via upload

---
+++ db.js
@@ -0,0 +1,181 @@
+
+  var vencedores = [
+        {
+          item: 1,
+          freguesia: 'Alcantara',
+          valor: "545.000€",
+          quartos: 2,
+          banheiro: 2,
+          m2: 117,
+          garagem: 'sim',
+          eficiencia: 'B-',
+          imobiliaria: 'BM Home',
+          maps:'',
+          link:'https://casa.sapo.pt/comprar-apartamento-t2-lisboa-alcantara-rio-perto-escola,centro-da-cidade,policia,transportes-publicos-com-garagem-estacionamento-tem-hall-de-entrada,ar-condicionado,comercio-d4cd0555-afb6-11ea-9de6-060000000057.html',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        },
+        {
+          item: 2,
+          freguesia: 'Belém',
+          valor: 550.000,
+          quartos: 2,
+          banheiro: 2,
+          m2: 118,
+          garagem: 'sim',
+          eficiencia: 'B-',
+          imobiliaria: 'jll',
+          maps:'',
+          link:'https://casa.sapo.pt/comprar-apartamento-t2-lisboa-belem-perto-escola,transportes-publicos-com-garagem-estacionamento,piscina,terraco-tem-varandas,condominio-fechado,arrecadacao-5cd56fc4-b070-11ea-95df-060000000053.html',
+          descricao:'Apartamento T2 com 120 m2 e terraço de 30 m2, inserido em condomínio com piscina em Belém, Lisboa. Boa distribuição dos espaços e excelentes áreas. A dois minutos do Rio Tejo e próximo do Mosteiro dos Jerónimos, Centro Cultural de Belém e Fundação Champalimaud. Dois lugares de garagem e arrecadação.'
+        },
+        {
+          item: 3,
+          freguesia: 'Santo António',
+          valor: 505.000,
+          quartos: 2,
+          banheiro: 1,
+          m2: 67,
+          garagem: 'não',
+          eficiencia: 'B-',
+          imobiliaria: 'Iad',
+          maps:'',
+          link:'http://lucianocordeiro118.com/#localizacao',
+          descricao:'Apartamento T2 de 67m2 inserido no Projecto Luciano Cordeiro 118, que resulta da reabilitação de um edifício cheio de histórias e pormenores arquitetónicos. Este projecto de autor acentua as características originais dos espaços, revestindo-os de sofisticação e originalidade.Luz natural. Linhas retas e elegantes. Serenidade.Cada apartamento tem acabamentos exclusivos que lhe oferecem uma harmonia e estética diferenciadoras. minimalista e conjugável com diferentes tipos de decoração,Com acabamentos exclusivos, funcionais, práticos, mas com um sentido estético inconfundível.O interior do prédio foi concebido como um jardim interior suspenso, e todos os apartamentos estão orientados em torno desta visão de verde.Este T2 fica num quarto andar e a sua cozinha americana envidraçada permite vislumbrar todo este ambiente tranquilo de jardim. Tem ainda dois quartos e uma casa de banho,O aparttamento será vendido totalmente equipado e o final da obra está previsto para o final de 2020.(Cedência de Posição).Localização Ímpar junto â Praça do Marquês de Pombal, um excelente investimento para rentalbilizar ou para habitação própria de quem valoriza poder morar mesmo no coração da cidade para a disfrutar em pleno.Rodeada de transportes e serviços, insere-se numa zona onde é permitido desenvolvier a actividade de arrendamento local.'
+        },
+        {
+          item: 4,
+          freguesia: 'Lumiar',
+          valor: 550.000,
+          quartos: 2,
+          banheiro: 2,
+          m2: 124,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'ERG Real State',
+          maps:'',
+          link:'https://www.imovirtual.com/anuncio/t2-exclusivo-quinta-dos-alcoutins-paco-do-lumiar-ID13MqC.html#5d0c80b96e',
+          descricao:'Apartamento T2, Novo, na Quinta dos Alcoutins, com 2 lugares de estacionamento e arrecadação. A 6km do aeroporto'
+        },
+        {
+          item: 5,
+          freguesia: 'Avenidas Novas',
+          valor: 540.000,
+          quartos: 2,
+          banheiro: 2,
+          m2: 104.65,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'RioMAgic',
+          maps:'',
+          link:'https://www.imovirtual.com/anuncio/apartamento-t2-com-estacionamento-na-av-republica-em-lisboa-ID12sKr.html#51cf08bf9c',
+          descricao:'Apartamento T2 Novo em Venda, composto por Sala (Varanda), Kitchenette, Suite (WC privativa), WC social, Hall, Arrecadação e 1 Estacionamento.Inserido em Edifício com arquitectura inovadora, com aplicação de materiais modernos que privilegiam a Sustentabilidade Ambiental, Eficiência Energética e Térmica.Localização privilegiada (próx. Campo Pequeno), acesso directo à Av. República, eixo principal da cidade de Lisboa.Próximo de Comércio, Serviços, Lazer, Saúde, Metro e Comboio (Entrecampos).'
+        },
+        {
+          item: 6,
+          freguesia: 'Arroios',
+          valor: 550.000,
+          quartos: 2,
+          banheiro: '2',
+          m2: 82,
+          garagem: 'não',
+          eficiencia: 'B-',
+          imobiliaria: 'PF Real State',
+          maps:'',
+          link:'https://casa.sapo.pt/comprar-apartamento-t2-lisboa-arroios-intendente-(anjos)-perto-centro-da-cidade,transportes-publicos-tem-varanda,ar-condicionado-9222edde-ccd4-11ea-95df-060000000053.html',
+          descricao:''
+        },
+        {
+          item: 7,
+          freguesia: 'Parque da Nações',
+          valor: 550.000,
+          quartos: 2,
+          banheiro: '2',
+          m2: 125,
+          garagem: 'sim',
+          eficiencia: 'B',
+          imobiliaria: 'Century 21',
+          maps:'',
+          link:'https://www.imovirtual.com/anuncio/apartamento-t2-no-parque-das-nacoes-no-condominio-metrocity-ID13kqo.html#7c4ac449d0',
+          descricao:'Apartamento T2 inserido no condomínio fechado Metrocity no Parque das Nações .Com uma disposição solar nascente/poente e situado de frente para a piscina do empreendimento este apartamento é moderno ,bem distribuído encontrando-se em excelente estado.Tem ar condicionado e aquecimento eléctrico em todas as divisões. Com um lugar de estacionamento e 1 arrecadação. O condomínio Metrocity tem espaços comuns de jardim , parque infantil , piscina e portaria com vigilância 12 horas. Tem ainda uma localização privilegiada junto a zona comercial do Parque das Nações,serviços,transportes,metro,Centro comercial Vasco da Gama e Gare do Oriente.'
+        },
+        {
+          item: 8,
+          freguesia: 'Santo António',
+          valor: 530.000,
+          quartos: 2,
+          banheiro: '2',
+          m2: 80,
+          garagem: 'não',
+          eficiencia: 'B',
+          imobiliaria: 'Iad',
+          maps:'',
+          link:'https://www.idealista.pt/pro/iad-portugal/imovel/30004040/',
+          descricao:'Este moderno apartamento localiza-se a dois passos na AV da Liberdade e do Marquês de Pombal, perto do Saldanha e do Campo dos Mártires da Pátria. Localizado no 3º andar de um prédio de época com elevador, espaçosas assoalhadas, com uma varanda que o acompanha, onde poderá desfrutar do cosmopolitismo da cidade. A generosas áreas da sala de estar com moderna cozinha em open-space, 2 quartos, um em suite, 2 varandas, ar condicionado, eletricidade completa e um amplo terraço com espaço para refeições. O apartamento é inteligente e possui tecnologia e equipamentos controlados por telefone. A renovação do apartamento e realização da alta qualidade do acabamento e do equipamento.'
+        },
+        {
+          item: 9,
+          freguesia: 'São Domingos de Benfica',
+          valor: 512.500,
+          quartos: 2,
+          banheiro: '2',
+          m2: 108,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'Castelhana',
+          maps:'',
+          link:'https://www.imovirtual.com/anuncio/excelente-apartamento-t2-a-estrear-no-alto-dos-moinhos-co-ID13pt5.html#27f788b996',
+          descricao:'Excelente apartamento T2 a estrear no Alto dos Moinhos com estacionamento e arrecadação. O apartamento insere-se num edifício totalmente novo cuja obra terminou recentemente. Localizado num 1º andar bastante alto, funcionando para a frente ao nível de um 2º andar e para trás ao nível de um 4º andar. A tardoz conta com uma simpática varanda com vista desafogada para Monsanto! Acabamentos de qualidade superior: Isolamento acústico e térmico; Ar- condicionado; Painéis solares; Electrodomésticos de encastre topo de gama; Estores eléctricos com corte térmico; Vidros duplos com corte térmico; Detector de fumos e de gás. A poucos metros de: Metro Alto dos Moinhos; Hospital dos Lusíadas; Externato Marista de Lisboa; Estádio da Luz.'
+        },
+        {
+          item: 10,
+          freguesia: 'Benfica',
+          valor: 550.000,
+          quartos: 3,
+          banheiro: '2',
+          m2: 130,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'Albuquerque & Simões',
+          maps:'',
+          link:'https://www.imovirtual.com/anuncio/t3-benfica-condominio-fechado-ID13PdP.html',
+          descricao:'Inserido num condomínio fechado com piscina e um amplo jardim, onde crescem árvores de fruto. Apartamento com pré-instalação de domótica e cozinha completamente equipada e ainda aspiração central e detectores de inundação, gás e incêndio. Uma sala ampla com A/C e virada para a piscina. Tem uma casa de banho de apoio com poliban. No corredor de acesso tem 2 roupeiros de apoio e todos quartos tem roupeiros e A/C, sendo um deles uma suite com uma banheira de hidomassagem. Tem 3 lugares de estacionamento e uma arrecadação. Zona perto de transportes públicos.'
+        },
+        {
+          item: 11,
+          freguesia: 'Lumiar',
+          valor: "537.000€",
+          quartos: 3,
+          banheiro: '3',
+          m2: 140,
+          garagem: 'sim',
+          eficiencia: 'B',
+          imobiliaria: 'Daniel',
+          maps:'',
+          link:'https://www.idealista.pt/imovel/30733094/',
+          descricao:'Apartamento T3 em uma zona residencial muito tranquila. Junto ao Parque da Quinta das Conchas com vista sobre o mesmo. Próximo ao mercado, hospital Pulido Valente, farmácia, cafés. A poucos minutos (a pé) da estação do metro Quinta das Conchas. Possui dispensa e arrecadação'
+        },
+        {
+          item: 11,
+          freguesia: '',
+          valor: "537.000",
+          quartos: 2,
+          banheiro: '2',
+          m2: 0,
+          garagem: 'sim',
+          eficiencia: 'B-',
+          imobiliaria: 'Daniel',
+          maps:'',
+          link:'',
+          descricao:''
+        },
+  ];
+      var podioPorPais = vencedores.map(function(item, indice){
+        return `<tr>
+                    <td>${item.item}</td>
+                    <td>${item.freguesia}</td>
+                    <td>${item.valor}</td>
+                    <td><a href=${item.link}>click</a></td>
+                </tr>`;
+       });
+       
+       
