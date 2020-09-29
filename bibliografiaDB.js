ceb4bfc96b994bc2d0a18e26a8171367b320c843
marcleyc <marcleycc@gmail.com>
2020-09-29 23:10:28 +0100

    Add files via upload

---
+++ bibliografiaDB.js
@@ -0,0 +1,112 @@
+var bibliografia = [
+{situacao:"", autor:"Manoel Lopes Porto", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"Maria Matilde Costa Lavouras Francisco", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"Vitor João de Vasconcelos Raposo Ribeiro Calvete", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"Teresa Maria Coelho Marques de Almeida Moura Ramos", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"Felipe Figueiredo Martins", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"João José Nogueira de Almeida", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"João Nuno Cruz Matos Calvão da Silva", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{situacao:"", autor:"Miguel Maria Tavares Festas Gorjão ", titulo:"", pesquisar:"", ano:2, url:"", editora:""},
+{
+ situacao:"U", 
+ autor:"Fragoso Silvio",
+ titulo:"Ordem pública no Mercosul e na união Europea: critérios de aplicação",
+ pesquisar:"Fragoso Silvio 'Ordem pública no Mercosul e na união Europea: critérios de aplicação'", 
+ ano:2,
+ url:"",
+ editora:"Editora Juruá 2019 (1976)",
+ uc:"UC Faculdade de Direito D-12-127-25"
+},   
+{
+ situacao:"U", 
+ autor:"Adalberto Perulli",
+ titulo:"Sustainable Development Global Trade an Social Rights",
+ pesquisar:"", 
+ ano:2,
+ url:"",
+ editora:"",
+ uc:"UC D-12-115-18 - 161346"
+},   
+{
+ situacao:"X", 
+ autor:"Cienfuegos Mateo Manuel",
+ titulo:"Opiniones consultivas en el Mercosur y cuestiones prejudiciales en la Unión Europea",
+ pesquisar:"Cienfuegos Mateo Manuel 'Opiniones consultivas en el Mercosur y cuestiones prejudiciales en la Unión Europea'", 
+ ano:2012,
+ url:"https://recyt.fecyt.es/index.php/RDCE/article/view/39517",
+ editora:"Revista de Derecho Comunitario Europeo nº 12 (2012) p433-476"
+},         
+{
+ situacao:"X", 
+ autor:"Deborah Barros Leal Farias",
+ titulo:"Taking advantage of others' protecionism and isolationism - the case Mercosul",
+ pesquisar:"Deborah Barros Leal Farias 2019 - Taking advantage of others' protecionism and isolationism - the case Mercosul", 
+ ano:2019,
+ url:"https://doi.org/10.1080/11926422.2019.1642217",
+ editora:"Canadian Foreign Policy Journal"
+}, 
+{
+ situacao:"N", 
+ autor:"",
+ titulo:"THE BRAZILIAN CASE AND THE MERCOSUL",
+ pesquisar:"", 
+ ano:2,
+ url:"http://ojs.ual.es/ojs/index.php/eea/article/view/2606",
+ editora:""
+},
+{
+ situacao:"N", 
+ autor:"",
+ titulo:"",
+ pesquisar:"", 
+ ano:2019,
+ url:"https://internationalsugarjournal.com/?s=Mercosur+trade+deal",
+ editora:""
+},
+{
+ situacao:"U", 
+ autor:"Abreu Jorge Manuel Coutinho",
+ titulo:"Curso de Direito Comercial: Das Sociedades",
+ pesquisar:"Abreu Jorge Manuel Coutinho 'Curso de Direito Comercial: Das Sociedades'", 
+ ano:2016,
+ url:"",
+ editora:"Almedina",
+ uc:"UC Faculdade Economia	D 347.7 ABR vol.1"
+},
+{
+ situacao:"X", 
+ autor:"Cruz José N",
+ titulo:"A influência do diretio no empreededorismo em Portugal",
+ pesquisar:"", 
+ ano:2014,
+ url:"https://doi.org/10.14195/0870-4260_57-1_34",
+ editora:"Boletim de Ciências Económicas, Coimbra v57 p1231-1266"
+},
+{
+ situacao:"U", 
+ autor:"Colaço Luís Filipe Antunes",
+ titulo:"O Direito Administrativo Sem Estado. Crise ou Fim de um Paradigma?",
+ pesquisar:"", 
+ ano:2,
+ url:"",
+ editora:"Editora Coimbra",
+ uc:"Faculdade Direito J-117-20-52, Biblioteca Geral 9-(1)-8-7-38 c.5"
+},
+{
+ situacao:"U", 
+ autor:"Manuel C. Lopes Porto",
+ titulo:"Teoria da Integração e políticas comunitárias",
+ pesquisar:"", 
+ ano:2001,
+ url:"",
+ editora:"Coimbra 3ª edição, pag.223-225",
+ uc:"UC Biblioteca Geral 6-50 A-2-40"
+},  
+{situacao:"U", autor:"Bhagwati Jagdish", titulo:"Writings on International Economics", pesquisar:"", ano:1997, url:"", editora:"Oxford University Press. pag.168",uc:" Fac.Direito J-56-7, Fac.Economia 339.9 BHA"},
+{situacao:"U", autor:"Elias Soukiazis", titulo:"Integração económica", ano:2003, url:"", editora:"",uc:"Fac.Economia D 339.9 SOU"},
+{situacao:"U", autor:"José Pedro Teixeira Fernandes", titulo:"Elementos de economia política internacional", ano:2005, url:"", editora:"",uc:"Fac.Economia D 339.9 FER  & BG 8-(2)-28-37-6 c.2"},
+{situacao:"U", autor:"Eduardo Raposo de Medeiros", titulo:"Evolução e tendências da integração económica regional", ano:2008, url:"", editora:"",uc:"Fac.Direito J-22-4-23 & BG 9-(1)-7-42-50 c.2"},
+{situacao:"U", autor:"Bela Balassa", titulo:"Teoria da Integração Económica", ano:1961, url:"", editora:"",uc:"Fac.Economia FTR 1328"},
+{situacao:"U", autor:"Sanford Gaines", titulo:"Liberalising trade in the EU and the WTO : a legal comparison", ano:2012, url:"http://search.ebscohost.com/login.aspx?direct=true&AuthType=cookie,ip,shib,uid&db=cat01014a&AN=ibu.b2566551&lang=pt-pt&site=eds-live", editora:"",uc:"Fac.Direito	J-56-30-18"}, 
+{situacao:"", autor:"", titulo:"", ano:2, url:"", editora:"",uc:""},               
+]
